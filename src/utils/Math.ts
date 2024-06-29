d1264381748ccb98d5af95586cf2252cf6119618
Taha Shashtari <taha.shashtari@gmail.com>
2024-06-29 17:51:57 +0300

    add randomNumber to Utils

--- src/utils/Math.ts
+++ src/utils/Math.ts
@@ -5,6 +5,10 @@ export function clamp(num: number, min: number, max: number): number {
   return Math.min(Math.max(num, min), max)
 }
 
+export function randomNumber(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 export function valueAtPercentage(
   from: number,
   to: number,
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,8 @@
 export { minBy } from './Finders'
-export { clamp, valueAtPercentage, remap, pointToViewProgress } from './Math'
+export {
+  clamp,
+  valueAtPercentage,
+  remap,
+  pointToViewProgress,
+  randomNumber
+} from './Math'
